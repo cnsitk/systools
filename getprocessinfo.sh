36a7d69c9527a596ab1c817258db1a2faf2a0e6d
SITK MING <cnsitk@outlook.com>
2022-08-09 15:10:11 +0800

    修正进程CPU使用率计算
    有可能会偶尔会超过100%

--- getprocessinfo.sh
+++ getprocessinfo.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-TestPID=""
-FilterName="$1"
+TestPID="$1"
 
-if [ -z "$FilterName" ]; then
-    echo "pls add the progress name filter prams!"
-    exit -1 
-fi
-TestPID=`ps aux | grep "$FilterName" | grep -v 'grep'  | head -n 1 | awk '{print $2}'` 2>/dev/null
-TestPgName=`ps aux | grep "$FilterName" | grep -v 'grep'  | head -n 1 | awk '{print $11}' | awk  -F '/' '{print $2}'` 2>/dev/null
 
 if [ -z $TestPID ];  then
-   echo "Progress Not Found!"
+   echo "Pls add the Progress ID!"
    exit -1
 fi
+
 cpu_cores=$(getconf _NPROCESSORS_ONLN)
 clock_ticks=$(getconf CLK_TCK)
 total_memory=$( grep -Po '(?<=MemTotal:\s{8})(\d+)' /proc/meminfo )
-stat_array=( `sed -E 's/(\([^\s)]+)\s([^)]+\))/\1_\2/g' /proc/$TestPID/stat` )
-uptime_array=( `cat /proc/uptime` )
-uptime=${uptime_array[0]}
-utime=${stat_array[13]}
-stime=${stat_array[14]}
-cutime=${stat_array[15]}
-cstime=${stat_array[16]}
-num_threads=${stat_array[19]}
-if [[ $num_threads == 0 ]]; then 
-  num_threads= `grep "Threads:" /proc/$TestPID/status 2>/dev/null | awk '{print $2}'`
-fi
+
 mem_rss=`grep "Rss:" /proc/$TestPID/smaps | awk 'BEGIN {a=0 } {a = a+$2} END {print a}'`
 mem_pss=`grep "Pss:" /proc/$TestPID/smaps | awk 'BEGIN {a=0 } {a = a+$2} END {print a}'`
 mem_swap=`grep "Swap:" /proc/$TestPID/smaps | awk 'BEGIN {a=0 } {a = a+$2} END {print a}'`
 mem_ref=`grep "Referenced:" /proc/$TestPID/smaps | awk 'BEGIN {a=0 } {a = a+$2} END {print a}'`
 memory_usage=$( awk 'BEGIN {print( ('$mem_rss'  * 100) / '$total_memory'  )}' )
-starttime=${stat_array[21]}
-total_time=$(( $utime + $stime ))
-total_time=$(( $total_time + $cstime ))
-seconds=$( awk 'BEGIN {print ( '$uptime' - ('$starttime' / '$clock_ticks') )}' )
-cpu_usage=0
-if  expr $seconds '>' 0 1>/dev/null; then
-  cpu_usage=$( awk 'BEGIN {print ( 100 * (('$total_time' / ('$clock_ticks' * '$cpu_cores')) / '$seconds') )}' )
-fi
 
-printf "MemInfo(KB):Total:$total_memory  RSS:$mem_rss PSS:$mem_pss Swap:$mem_swap Ref:$mem_ref Usage:%.4f\n" $memory_usage 
-printf "CpuInfo(%%):Threads:$num_threads Usage:%.4f\n" $cpu_usage 
+stat_array=`cat /proc/$TestPID/stat | cut -d' ' -f14-20`
+utime=`echo $stat_array | cut -d' ' -f1`
+stime=`echo $stat_array | cut -d' ' -f2`
+cutime=`echo $stat_array | cut -d' ' -f3`
+cstime=`echo $stat_array | cut -d' ' -f4`
+total_time01=$(( $utime + $stime + $cstime + $cutime ))
+num_threads=`echo $stat_array | cut -d' ' -f7`
+
+sleep_interanl=1
+sleep $sleep_interanl
+
+stat_array=`cat /proc/$TestPID/stat | cut -d' ' -f14-20`
+utime=`echo $stat_array | cut -d' ' -f1`
+stime=`echo $stat_array | cut -d' ' -f2`
+cutime=`echo $stat_array | cut -d' ' -f3`
+cstime=`echo $stat_array | cut -d' ' -f4`
+total_time02=$(( $utime + $stime + $cstime + $cutime ))
+num_threads=`echo $stat_array | cut -d' ' -f7`
+
+cpu_usage=$( awk 'BEGIN {print ( 100 * ( ('$total_time02'  - '$total_time01') / '$clock_ticks')  / ('$sleep_interanl' * '$cpu_cores')  )}' )
+
+printf "MemInfo(KB):Total:$total_memory  RSS:$mem_rss PSS:$mem_pss Swap:$mem_swap Ref:$mem_ref Usage:%.4f\n" $memory_usage
+printf "CpuInfo(%%):Threads:$num_threads Usage:%.4f\n" $cpu_usage
 exit 0
